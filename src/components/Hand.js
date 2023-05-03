6ae0341b35d7e7fc42d0f3ec0ddd8695b1f9bc66
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-03 05:47:54 -0500

    playing with design. react is weird.

---
+++ src/components/Hand.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Deck from './Deck';
+
+
+function Hand() {
+
+    const [hand, setHand] = useState([]);
+
+
+  return (
+    <div>
+        <Hand />
+    </div>
+  )
+}
+
+export default Hand
