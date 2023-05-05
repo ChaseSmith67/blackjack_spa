afdcb81e2a48fb98f93c013187175b68768fdc21
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-04 20:56:51 -0500

    remove useState for deck.

--- src/App.js
+++ src/App.js
@@ -5,29 +5,47 @@ import React, { useState, useEffect } from "react";
 function App() {
 
   const newDeck = () => {
-    const deck = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
+    let deck = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
                   'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
                   'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
                   'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS'];
     return deck;
   };
 
+  // Fisher-Yates algorithm to shuffle the deck
+  const shuffleArray = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+
+  // const shuffleDeck = () => {
+  //   shuffleArray(deck);
+  //   return deck;
+  // };
+
   const [dealerHand, setDealerHand] = useState([]);
   const [playerHand, setPlayerHand] = useState([]);
-  const [deck, setDeck] = useState(newDeck());
+  // const [deck, setDeck] = useState(shuffleArray(newDeck()));
 
-  const drawCard = () => {
-    let card = deck.pop(Math.floor(Math.random() * deck.length));
-    setDeck(deck);
+
+  function drawCard(deck) {
+    let card = deck.pop();
+    console.log(deck);
+    // setDeck(deck);
     return card;
   };
 
-  const dealPlayer = () => {
-    setPlayerHand(playerHand => [...playerHand, drawCard()]);
+  function dealPlayer(deck) {
+    setPlayerHand(playerHand => [...playerHand, drawCard(deck)]);
   };
 
-   const dealDealer = () => {
-    setDealerHand(dealerHand => [...dealerHand, drawCard()]);
+   function dealDealer(deck) {
+    setDealerHand(dealerHand => [...dealerHand, drawCard(deck)]);
    };
 
    const newHand = () => {
@@ -35,11 +53,17 @@ function App() {
     setDealerHand([]);
     setPlayerHand([]);
 
+    // Create a new deck and shuffle
+    let deck = shuffleArray(newDeck());
+    // setDeck(() => shuffleArray(newDeck()));
+    // console.log(deck);
+    // console.log(freshDeck);
+
     // Deal Cards
-    dealPlayer();
-    dealDealer();
-    dealPlayer();
-    dealDealer();
+    dealPlayer(deck);
+    dealDealer(deck);
+    dealPlayer(deck);
+    dealDealer(deck);
 
    };
 
