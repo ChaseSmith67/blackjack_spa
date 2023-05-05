87de78d817a8e91ac34225c4aa55b1a30db0bb93
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-04 23:08:45 -0500

    setting up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 // import Deck from "./components/Deck";
 // import Hand from "./components/Hand";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
