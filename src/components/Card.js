9c9db34b443da5ee7d5fe7eacd16191f5528f9bc
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-08 00:05:57 -0500

    build card class

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,2 +1,20 @@
 
+class Card {
+    constructor(value, suit, image, visible = true) {
+        this.value = value;
+        this.suit = suit;
+        this.image = image;
+        this.visible = visible;
+    }
+}
+
+const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let deck = [];
+
+for (let suit of suits) {
+    for (let value of values) {
+        deck.push(new Card(value, suit, "./images" + suit + "/" + value + ".png"));
+    }
+}
 
