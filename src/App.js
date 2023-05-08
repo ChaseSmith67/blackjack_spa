b1f561d3b8bff0ff280c79e1511754f49087966e
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-08 10:29:21 -0500

    Card Images working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from "react";
 // import Deck from "./components/Deck";
-// import Hand from "./components/Hand";
+// import "./components/Card.js";
 import './App.css';
 import Container from 'react-bootstrap/Container';
+
+
+class Card {
+  constructor(value, suit, image, visible = true) {
+      this.value = value;
+      this.suit = suit;
+      this.image = image;
+      this.visible = visible;
+  }
+}
 
 function App() {
 
   const newDeck = () => {
-    let deck = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
-                  'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
-                  'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
-                  'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS'];
+    // let deck = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
+    //               'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
+    //               'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
+    //               'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS'];
     
+    // src/images/Diamonds/2.png
+
+    const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    let deck = [];
+
+    for (let suit of suits) {
+      for (let value of values) {
+          deck.push(new Card(value, suit, "images/" + suit + "/" + value + ".png"));
+      }
+    }
     // Fisher-Yates algorithm to shuffle the deck
     const shuffleArray = arr => {
       for (let i = arr.length - 1; i > 0; i--) {
@@ -99,11 +120,13 @@ function App() {
     };
   }, [dealerHand.cards, dealerHand.value]);
 
-  useEffect(() => {
-    if (playerTurn &&!gameOver) {
-      dealerHand.cards[0] = "??";
-    }
-  }, [playerTurn, gameOver]);
+  /* This is to hide the dealer's first card. Haven't totally
+      worked it out yet. */
+  // useEffect(() => {
+  //   if (playerTurn &&!gameOver) {
+  //     dealerHand.cards[0] = "??";
+  //   }
+  // }, [playerTurn, gameOver]);
 
   useEffect(() => {
     console.log('message', message);
@@ -111,7 +134,7 @@ function App() {
 
 
   const evalCard = (card) => {
-    switch (card[0]) {
+    switch (card.value) {
       case "K":
       case "Q":
       case "J":
@@ -119,10 +142,8 @@ function App() {
         return 10;
       case "A":
         return 11;
-      case "?":
-        return 0;
       default:
-        return parseInt(card[0]);
+        return parseInt(card.value);
     };
   };
 
@@ -205,7 +226,9 @@ function App() {
       <Container className="p-5 mb-4 bg-light rounded-3">
         <h1 className="header">Dealer's Cards</h1>
           <div className="d-flex justify-content-center">
-          {dealerHand.cards.map( e => (<td><Container className="card"> {e} </Container></td>))}
+          {dealerHand.cards.map( e => (<td><Container className="card p-0">
+             <img src={e.image} alt="" width="100" height="150"/>
+              </Container></td>))}
           </div>
           <div className="d-flex justify-content-center">{ dealerHand.value }</div>
       </Container>
@@ -214,7 +237,9 @@ function App() {
       <Container className="p-5 mb-4 bg-light rounded-3">
         <h1 className="header">Your Cards</h1>
           <div className="d-flex justify-content-center">
-          {playerHand.cards.map( e => (<td><Container className="card"> {e} </Container></td>))}
+          {playerHand.cards.map( e => (<td><Container className="card p-0"> 
+          <img src={e.image} alt="" width="100" height="150"/>
+           </Container></td>))}
           </div>
           <div className="d-flex justify-content-center">{ playerHand.value }</div>
       </Container>
