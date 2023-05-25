b77cec520fdc1a21975c5385fe834a90aecb1907
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-25 10:10:41 -0500

    Add basic documentation

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,6 +1,17 @@
+const numberOfDecks = 1; // Easily change number of decks
+
 
 class Card {
+    /** 
+    * Represents a single playing card.
+    */
     constructor(value, suit, image, visible = true) {
+        /**
+         * @param {string} value - String representation of the value of the card.
+         * @param {string} suit - String representation of the card's suit.
+         * @param {string} image - The file path to the card's image.
+         * @param {boolean} visible - Whether the card is face-up or not.
+         */
         this.value = value;
         this.suit = suit;
         this.image = image;
@@ -8,17 +19,27 @@ class Card {
     }
 }
 
+
+/** 
+ *  Build the deck by looping through the suits and values
+ *  and creating a new Card object and pushing it to the deck
+ *  array. The cards are then shuffled and the deck is returned.
+ */
 const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
 const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 let deck = [];
 
-for (let suit of suits) {
-    for (let value of values) {
-        deck.push(new Card(value, suit, "images/" + suit + "/" + value + ".png"));
+for (let i = 0; i < numberOfDecks; i++) {
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push(new Card(value, suit, "images/" + suit + "/" + value + ".png"));
+        }
     }
 }
 
-// Fisher-Yates algorithm to shuffle the deck
+/**
+ *  Fisher-Yates algorithm to randomly shuffle cards.
+ */  
 const shuffleArray = (arr) => {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
