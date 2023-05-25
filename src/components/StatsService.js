bdde3f91016d7a427388ff8e7ea7a2358f1cd350
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-05-25 10:14:28 -0500

    comment

--- src/components/StatsService.js
+++ src/components/StatsService.js
@@ -1,21 +1,16 @@
+/**
+ *  Retrieve statistics from the database via API call.
+ */
 
-  // API URL
-  const apiURL = 'http://localhost:3033/win/loss';
+const apiURL = 'http://localhost:3033/win/loss'; // API URL
 
-  export default async function getBlackJackStats() {
-    const response = await fetch(apiURL);
-    if (response.ok) {
-      const data = await response.json();
-      // console.log('Data: ', data);
-      // console.log(JSON.stringify(data));
-      // let stats = [];
-      // for (let i = 0; i < data.length; i++) {
-      //   stats.push(data[i].title, data[i].count)
-      // }
-      // return stats;
-      return data;
-    }
+export default async function getBlackJackStats() {
+  const response = await fetch(apiURL);
+  if (response.ok) {
+    const data = await response.json();
+    return data;
   }
+}
 
 
 
