bccbee455282fe4404c03e121d5242f0b9bb3546
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-06-04 15:14:35 -0500

    Remove old Stats page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Tutorial from "./pages/Tutorial";
-import Statistics from "./pages/Statistics";
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
             <Routes>
                 <Route exact path='/' element={<Game />} />
                 <Route path='/tutorial' element={<Tutorial />} />
-                <Route path='/statistics' element={<Statistics />} />
             </Routes>
         </Router>
     );
