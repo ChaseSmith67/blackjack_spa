e08c808a9677dfde9b9d03c76d5087c2e8f69751
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-06-04 17:33:29 -0500

    Finish writing Tutorial page

--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -4,25 +4,20 @@ import { Container, Row, Col } from "react-bootstrap";
 function Tutorial() {
   return (
     <Container className="p-1">
-      <Container className="p-3 mb-4 bg-info rounded-3">
+      <Container className="p-3 mb-4 bg-success rounded-3">
         <Row>
           <Col>
             <div className="d-flex justify-content-start">
               <h1>&spades; &hearts; &clubs; &diams;</h1>
             </div>
           </Col>
-          <Col xs={8} className="header p-3">
-            <h2>
+          <Col  xs={6} className="p-3">
+            <h2 className="header">
               <strong>How to Play Blackjack</strong>
             </h2>
-          </Col>
-          <Col>
-            <div className="d-flex justify-content-end">
-              <h1>&spades; &hearts; &clubs; &diams;</h1>
-            </div>
-          </Col>
-        </Row>
-
+          
+        
+        
         <div className="d-flex justify-content-center p-3">
           <h3> Basic Rules </h3>
         </div>
@@ -54,7 +49,7 @@ function Tutorial() {
             </ul>
           </p>
         </div>
-        <div className="d-flex justify-content-center p-3">
+        <div className="d-flex justify-content-center">
           <h3> Card Values </h3>
         </div>
         <div className="d-flex justify-content-center">
@@ -63,13 +58,72 @@ function Tutorial() {
               <li>2 through 10 are worth their face value.</li>
               <li>Face cards &#40;J, K, Q&#41; are worth 10.</li>
               <li>
-                Aces are worth either 1 or 11, depending on the value of the
-                hand. &#40;If the hand value would exceed 21 with the Ace being
-                valued at 11, then the Ace will be worth 1.&#41;
+                Aces are worth either 1 or 11, depending on the value of the hand. </li>
+              <li>
+                &#40;If the hand value would exceed 21 with the Ace being
+                valued at 11, then the Ace will be worth 1&#41;.
               </li>
             </ul>
           </p>
         </div>
+        <div className="d-flex justify-content-center">
+          <h3> Gameplay </h3>
+        </div>
+        <div className="d-flex justify-content-center">
+          <p>
+            Start of the game.
+            <ul>
+              <li>
+                First, the player must place a bet. If the player wins the game,
+                they will receive the amount of the bet, plus keep the original bet.
+              </li>
+              <li>
+                &#40;If a player has Blackjack, they win 1.5 times their bet&#41;.
+              </li>
+              <li>
+                The player and the dealer are both given two cards. The
+                dealer's first card is face-down, so you cannot see its value.
+              </li>
+              </ul>
+              The player's turn then begins.
+              <ul>
+              <li>
+                The player then has the option to draw another card &#40;known as a "Hitting"&#41;
+                or the player can end their turn and receive no more cards &#40;known as "Standing"&#41;.
+              </li>
+              <li>
+                The player can continue to hit until they either have a hand they are satisfied
+                with or they bust, losing the game.
+              </li>
+              <li>
+                If the player feels they have a particularly good chance of winning the game by
+                drawing only one more card, they can "Double Down", doubling their bet and 
+                receiving only one more card.
+              </li>
+              </ul>
+                When the player's turn is over, the dealer begins their turn.
+              <ul>
+                <li>
+                  The dealer reveals their face-down card.
+                </li>
+                <li>
+                  If the dealer's hand value is less than 17, the dealer must hit.
+                </li>
+                <li>
+                  The dealer continues to hit until they reach at least 17 or bust.
+                </li>
+              </ul>
+              The hands are then compared and either the player the game, collecting
+              their winnings, or loses and forfeits their bet.
+          </p>
+        </div>
+        </Col>
+          <Col>
+            <div className="d-flex justify-content-end">
+              <h1>&spades; &hearts; &clubs; &diams;</h1>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
