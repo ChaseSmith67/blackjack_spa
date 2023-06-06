8c43d6be2b85a7479c4d0189a29e0c44ae5765f5
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-06-05 19:46:43 -0500

    add about page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Tutorial from "./pages/Tutorial";
+import About from "./pages/About";
 
 function App() {
+
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Game />} />
         <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function About() {
+  return (
+    <Container className="p-1">
+      <Container className="p-3 mb-4 bg-success rounded-3">
+        <Row>
+          <Col>
+            <div className="d-flex justify-content-start">
+              <h1>&spades; &hearts; &clubs; &diams;</h1>
+            </div>
+          </Col>
+          <Col  xs={6} className="p-3">
+
+        <div className="d-flex justify-content-center p-3">
+          <h3> This app was created by Chase Smith</h3>
+          </div>
+          <div className="d-flex justify-content-center p-3">
+          <a href="https://github.com/ChaseSmith67">
+             https://github.com/ChaseSmith67 </a>
+        </div>
+        </Col>
+          <Col>
+            <div className="d-flex justify-content-end">
+              <h1>&spades; &hearts; &clubs; &diams;</h1>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default About;
